9c2eaf7bd4b1fff11b84c89986c5b7bc1733d758
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-01-17 15:56:55 +0100

    first extra added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // REFERENCES
 
 const row = document.getElementsByClassName("row")[1];
+const cart = document.getElementById("cart");
+
+// VARIABLES
+
+const cartArray = [];
 
 // FUNCTIONS
 const createBookCards = function () {
@@ -27,7 +32,7 @@ const createBookCards = function () {
         const newCol = document.createElement("div");
         newCol.classList.add("col");
         newCol.innerHTML = `
-        <div class="card">
+        <div class="card shadow">
               <img
                 src="${book.img}"
                 class="card-img-top"
@@ -36,9 +41,9 @@ const createBookCards = function () {
               <div class="card-body">
                 <h5 class="card-title mb-3">${book.title}</h5>
                 <p class="card-text mb-2">Genre: <span>${book.category}</span></p>
-                <p class="card-text">Price: <span>${book.price}</span>€</p>
+                <p class="card-text">Price: <span class="price">${book.price}</span>€</p>
                 <div class="d-flex justify-content-evenly">
-                  <button class="btn btn-primary" onclick="addToCart()">
+                  <button class="btn btn-primary" onclick="addToCart(event)">
                     Add
                   </button>
                   <button class="btn btn-danger" onclick="removeCard(event)">
@@ -61,6 +66,45 @@ const removeCard = function (event) {
   event.target.closest(".col").remove();
 };
 
+const addToCart = function (event) {
+  // FIND THE RIGHT CARD AND RESPECTIVE TITLE AND PRICE
+  const cardTargeted = event.target.closest(".card-body");
+  //   console.log(cardTargeted);
+  const cardTitle =
+    cardTargeted.getElementsByClassName("card-title")[0].innerHTML;
+  //   console.log(cardTitle);
+  const cardPrice = cardTargeted.getElementsByClassName("price")[0].innerHTML;
+  //   console.log(cardPrice);
+  const cardInfo = { title: cardTitle, price: cardPrice };
+  //   console.log(cardInfo);
+
+  //   ADD THE INFORMATION TO LOCAL STORAGE
+  cartArray.push(cardInfo);
+  localStorage.setItem("cart", JSON.stringify(cartArray));
+
+  //   CART LI CREATION
+  const newLi = document.createElement("li");
+  newLi.classList.add("d-flex");
+  newLi.classList.add("text-start");
+  newLi.classList.add("justify-content-between");
+  newLi.innerHTML = `<span class="me-5">${cardTitle}</span><span>${cardPrice}€</span>`;
+  cart.appendChild(newLi);
+};
+
+const cartOnLoad = function () {
+  const cartElement = JSON.parse(localStorage.getItem("cart"));
+  cartElement.forEach((element) => {
+    const newLi = document.createElement("li");
+    newLi.classList.add("d-flex");
+    newLi.classList.add("text-start");
+    newLi.classList.add("justify-content-between");
+    newLi.innerHTML = `<span class="me-5">${element.title}</span><span>${element.price}€</span>`;
+    cart.appendChild(newLi);
+  });
+};
+
 // ON-GOING
 
 window.onload = createBookCards();
+
+window.onload = cartOnLoad();
